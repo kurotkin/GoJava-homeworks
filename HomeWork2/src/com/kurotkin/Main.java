52cf272fd06b4ace69c0c82c57f1ebfce28c6022
kurotkin <kurotkin@users.noreply.github.com>
2017-07-14 11:32:59 +0300

    Добавлен выбор варианта вывода на экран

--- HomeWork2/src/com/kurotkin/Main.java
+++ HomeWork2/src/com/kurotkin/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
         System.out.print("Сколько Вам полных лет: ");
         while (!scanIn.hasNextInt()) {
-            System.out.println("");
+            //System.out.println("");
             System.out.print("Введите целое число: ");
             scanIn.nextLine();
         }
@@ -24,23 +24,41 @@ public class Main {
         System.out.print("Введите Ваше хобби: ");
         String hobby = scanIn.nextLine();
 
+        System.out.println("Каким образом выводить результат? 1 - в столбик, 2 - в предложение, 3 - через '-'");
+        System.out.print("Введите число: ");
+        while (!scanIn.hasNextInt()) {
+            //System.out.println("");
+            System.out.print("Введите целое число: ");
+            scanIn.nextLine();
+        }
+        int caseOut = scanIn.nextInt();
+        scanIn.nextLine();
+
         printSeparator();
-        System.out.println("Имя: " + name);
-        System.out.println("Город: " + location);
-        System.out.println("Возраст: " + Integer.toString(age));
-        System.out.println("Хобби: " + hobby);
-        printSeparator();
-        System.out.printf("Человек по имени %s живет в городе %s.\n", name, location);
-        System.out.printf("Этому человеку %d лет и любит он заниматься %s.\n", age, hobby);
-        printSeparator();
-        System.out.print(name);
-        System.out.println(" - имя");
-        System.out.print(location);
-        System.out.println(" - город");
-        System.out.print(age);
-        System.out.println(" - возраст");
-        System.out.print(hobby);
-        System.out.println(" - хобби");
+        switch (caseOut) {
+            case 1 :
+                System.out.println("Имя: " + name);
+                System.out.println("Город: " + location);
+                System.out.println("Возраст: " + Integer.toString(age));
+                System.out.println("Хобби: " + hobby);
+                break;
+            case 2 :
+                System.out.printf("Человек по имени %s живет в городе %s.\n", name, location);
+                System.out.printf("Этому человеку %d лет и любит он заниматься %s.\n", age, hobby);
+                break;
+            case 3 :
+                System.out.print(name);
+                System.out.println(" - имя");
+                System.out.print(location);
+                System.out.println(" - город");
+                System.out.print(age);
+                System.out.println(" - возраст");
+                System.out.print(hobby);
+                System.out.println(" - хобби");
+                break;
+            default:
+                System.out.println("Не правильный выбор вывода");
+        }
         printSeparator();
     }
 
