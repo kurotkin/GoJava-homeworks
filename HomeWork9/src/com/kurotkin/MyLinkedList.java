3cbeca17653822aad68cdc68aa58b9c5d50025ea
kurotkin <kurotkin@users.noreply.github.com>
2017-08-21 15:54:36 +0300

    Added MyNode

--- HomeWork9/src/com/kurotkin/MyLinkedList.java
+++ HomeWork9/src/com/kurotkin/MyLinkedList.java
@@ -1,7 +1,81 @@
 package com.kurotkin;
 
+import java.util.LinkedList;
+
 /**
  * Created by Vitaly Kurotkin on 17.08.2017.
  */
-public class MyLinkedList {
+public class MyLinkedList<T> implements Lists<T> {
+    private MyNode<T> first;
+    private MyNode<T> last;
+    private int size = 0;
+
+    @Override
+    public void add(T value) {
+        MyNode<T> node;
+        if(first.equals(null)){
+            node = new MyNode<>(value, null, null);
+            first = node;
+            last = node;
+            size++;
+        } else {
+            node = new MyNode<>(value, null, last);
+            last.next = node;
+            last = node;
+            size++;
+        }
+    }
+
+    @Override
+    public void remove(int index) {
+        if(index == 0) {
+            MyNode<T> node = first.next;
+            node.prev = null;
+            first.next = null;
+            first.item = null;
+            first = null;
+            first = node;
+        } else if(index == size) {
+
+        }
+
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+            if(node == null)
+                throw new IndexOutOfBoundsException("Index: " + index + ", Size: "+ size());
+        }
+
+    }
+
+    @Override
+    public void clear() {
+        MyNode<T> iterator = first;
+        while (iterator != null){
+            if(iterator.next != null){
+                iterator = iterator.next;
+                iterator.prev.item = null;
+                iterator.prev.prev = null;
+                iterator.prev.next = null;
+                iterator.prev = null;
+            } else {
+                iterator.item = null;
+                iterator.prev = null;
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    public T get(int index) {
+        MyNode<T> node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+            if(node == null)
+                throw new IndexOutOfBoundsException("Index: " + index + ", Size: "+ size());
+        }
+        return (T) node;
+    }
 }
---
+++ HomeWork9/src/com/kurotkin/MyNode.java
@@ -0,0 +1,16 @@
+package com.kurotkin;
+
+/**
+ * Created by Vitaly Kurotkin on 21.08.2017.
+ */
+public class MyNode<T> {
+    public T item;
+    public MyNode<T> next;
+    public MyNode<T> prev;
+
+    public MyNode(T item, MyNode<T> next, MyNode<T> prev) {
+        this.item = item;
+        this.next = next;
+        this.prev = prev;
+    }
+}
