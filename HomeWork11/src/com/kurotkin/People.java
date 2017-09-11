0a5f3a1578db8672021c5f299b955bc70a945ad0
kurotkin <kurotkin@users.noreply.github.com>
2017-09-11 16:46:02 +0300

    Added 11 lesson

--- HomeWork11/src/com/kurotkin/People.java
+++ HomeWork11/src/com/kurotkin/People.java
@@ -1,7 +1,44 @@
 package com.kurotkin;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 /**
  * Created by Vitaly on 08.09.2017.
  */
-public class People {
+public class People implements Runnable{
+    private int peopleNumber;
+    private final Semaphore SEMAPHORE;
+    final Random random = new Random();
+
+    public People(int peopleNumber, Semaphore SEMAPHORE) {
+        this.peopleNumber = peopleNumber;
+        this.SEMAPHORE = SEMAPHORE;
+    }
+
+    @Override
+    public void run() {
+        if (SEMAPHORE.tryAcquire()){
+            System.out.println("[" + peopleNumber + "] пришел ко входу в библиотеку");
+        } else {
+            System.out.println("[" + peopleNumber + "] пришел ко входу в библиотеку\n" +
+                    "[" + peopleNumber + "] ждет у входа в библиотеку");
+        }
+
+        try {
+            SEMAPHORE.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("[" + peopleNumber + "] вошел в библиотеку");
+        System.out.println("[" + peopleNumber + "] читает книгу ");
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("[" + peopleNumber + "] читает книгу ");
+        SEMAPHORE.release();
+    }
 }
