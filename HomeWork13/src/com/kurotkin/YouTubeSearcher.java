00f4ce2cfc2145b0a73cc0127dbd35839a84cd39
kurotkin <kurotkin@users.noreply.github.com>
2017-09-20 16:32:55 +0300

    development

--- HomeWork13/src/com/kurotkin/YouTubeSearcher.java
+++ HomeWork13/src/com/kurotkin/YouTubeSearcher.java
@@ -1,7 +1,14 @@
 package com.kurotkin;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
- * Created by Андрей on 20.09.2017.
+ * Created by Vitaly Kurotkin on 20.09.2017.
  */
-public class YouTubeSearcher {
+public class YouTubeSearcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
 }
